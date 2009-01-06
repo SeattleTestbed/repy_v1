49c028d5118829c3a307abaa5f5f22f047bba431
armon <USER@DOMAIN>
2009-01-06 02:37:36 +0000

    Added new constant/variable REPY_CURRENT_DIR to repy_constants

--- repy_constants.py
+++ repy_constants.py
@@ -10,7 +10,11 @@ PATH_PYTHON_INSTALL = "\\Storage Card\\Program Files\\Python25\\python.exe"
 PYTHON_DEFAULT_FLAGS = "/new /nopcceshell "
 
 # Repy Installation path
-PATH_SEATTLE_INSTALL = "\\Storage Card\\Program Files\\Python25\\Lib\\REPY\\"
+#PATH_SEATTLE_INSTALL = "\\Storage Card\\Program Files\\Python25\\Lib\\SEATTLE\\"
+PATH_SEATTLE_INSTALL = "Z:\\trunk\\repy\\"
+
+# Current Working directory
+REPY_CURRENT_DIR = "."
 
 # Polling Frequency for different Platforms
 RESOURCE_POLLING_FREQ_LINUX = .1 # Linux can check relatively quickly
