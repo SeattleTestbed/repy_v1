c579226c3e03e8565cf585df4ca4e6000a97caf7
armon <USER@DOMAIN>
2009-01-30 23:41:25 +0000

    Increased Mobile CPU sample rate

--- repy_constants.py
+++ repy_constants.py
@@ -7,11 +7,13 @@ PATH_PYTHON_INSTALL = "\\Storage Card\\Program Files\\Python25\\python.exe"
 
 # Default Python Flags
 # e.g. The "/new" flag is necessary for PythonCE to allow multiple instances
+# The /nopcceshell disables the PythonCE window from opening and annoying the user
 PYTHON_DEFAULT_FLAGS = "/new /nopcceshell "
+#PYTHON_DEFAULT_FLAGS = ""
 
 # Repy Installation path
-#PATH_SEATTLE_INSTALL = "\\Storage Card\\Program Files\\Python25\\Lib\\SEATTLE\\"
-PATH_SEATTLE_INSTALL = "Z:\\trunk\\repy\\"
+PATH_SEATTLE_INSTALL = "\\Storage Card\\Program Files\\Python25\\Lib\\SEATTLE\\"
+#PATH_SEATTLE_INSTALL = "Z:\\Projects\\seattle\\trunk\\repy\\"
 
 # Current Working directory
 REPY_CURRENT_DIR = "."
@@ -26,4 +28,4 @@ RESOURCE_POLLING_FREQ_WINCE = 2 # Mobile devices are pretty slow
 # CPU Polling Frequency for different Platforms
 CPU_POLLING_FREQ_LINUX = .1 # Linux
 CPU_POLLING_FREQ_WIN = .1 # Windows
-CPU_POLLING_FREQ_WINCE = 1 # Mobile devices are pretty slow
+CPU_POLLING_FREQ_WINCE = .5 # Mobile devices are pretty slow
