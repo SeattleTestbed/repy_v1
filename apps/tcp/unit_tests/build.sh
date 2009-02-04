09f79aad52e8cf4832c75dd2fba819adb7659637
richard <USER@DOMAIN>
2009-02-04 16:56:30 +0000

    tcp: separating classes in different files

--- apps/tcp/unit_tests/build.sh
+++ apps/tcp/unit_tests/build.sh
@@ -3,16 +3,16 @@
 # Uses repypp to build tests.
 # Puts tests in $TESTDIR.
 
-REPYPP=../test/repypp.py
-TESTDIR=../test
+REPYPP=repypp.py
+TESTDIR=test
 
-cp ../tcp.repy .
+cp ../*.repy .
 echo "Building tests..."
 files=`ls [zne]_test*.py`
 for f in ${files}
 do
   echo ${f}
-  python ${REPYPP} ${f} ${TESTDIR}/${f}
+  python ../${TESTDIR}/${REPYPP} ${f} ../${TESTDIR}/${f}
 done
 echo "Done"
 
