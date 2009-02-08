a94942c48397c8ea46dcefb66a8a2a7e1704e94f
richard <USER@DOMAIN>
2009-02-08 16:50:55 +0000

    tcp: improved build to use now only make FN=<reg expression>

--- apps/tcp/tests/build.sh
+++ apps/tcp/tests/build.sh
@@ -3,7 +3,7 @@
 # Uses repypp to build tests.
 # Puts tests in $TESTDIR.
 
-: ${1?"Usage: $0 <output directory> [classname]"}
+: ${1?"Usage: $0 output_directory [files ...]"}
 
 REPYPP=../../../../seattlelib/repypp.py 
 TESTDIR=$1
@@ -13,13 +13,7 @@ then
   mkdir ../${TESTDIR}
 fi
 
-if [ -z "$2" ]
-then
-  files=`ls [zne]_test*.py`
-else
-  files=`ls [zne]_test_$2*.py`
-fi
-
+files=$2
 # copy in included files
 cp ../*.repy .
 echo "Building tests..."
