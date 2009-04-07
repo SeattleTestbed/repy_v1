0f758e723aab545ba7426255c9ee7753cd78c840
armon <USER@DOMAIN>
2009-04-07 00:39:08 +0000

    Ticket #330. Upgraded stopfilewatcher to parse stopfile and pick out stop code and message.

--- stopfilewatcher.py
+++ stopfilewatcher.py
@@ -6,6 +6,10 @@
    Description:
   
    Checks for the existance of a file and exits if it exists...
+   An attempt is made to parse the file for a string of the format
+   "EINT;MESG" where EINT is a string representation of an integer that is
+   passed to nonportable.harshexit and MESG is a string to be printed prior
+   to exiting. On any error, no message is printed and repy is exited with code 44 "Stopped"
 """
 
 import threading
@@ -20,6 +24,8 @@ import nonportable     # for harshexit
 #frequency to check
 frequency = 0.5
 
+# This is so that we can open the stopfiel
+myopen = open
 
 def init(stopfn):
   if os.path.exists(stopfn):
@@ -35,6 +41,36 @@ def checkfunction(stopfn):
   # run forever
   while True:
     if os.path.exists(stopfn):
-#      print "Stop file: '"+stopfn+"' exists!"
-      nonportable.harshexit(44)
+      try:
+        # Get a file object for the file
+        fileobject = myopen(stopfn)
+
+        # Read in the contents, close the object
+        contents = fileobject.read()
+        fileobject.close()
+          
+        # Check the length, if there is nothing then just close as stopped
+        if len(contents) > 0:
+          # Split, at most we have 2 parts, the exit code and message
+          (exitcode, mesg) = contents.split(";",1)
+          exitcode = int(exitcode)
+          
+          # Check if exitcode is 56, which stands for ThreadErr is specified
+          # ThreadErr cannot be specified externally, since it has side-affects
+          # such as changing global thread restrictions
+          if exitcode == 56:
+            raise Exception, "ThreadErr exit code specified. Exit code not allowed."
+          
+          # Print the message, then call harshexit with the exitcode
+          if mesg != "": 
+            print mesg
+          nonportable.harshexit(exitcode)
+          
+        else:
+          raise Exception, "Stopfile has no content."
+          
+      except:
+        # On any issue, just do stopped
+        nonportable.harshexit(44)
+      
     time.sleep(frequency)
