d7675504a19ffd9e99628132b1a4d5f71c55de6a
armon <USER@DOMAIN>
2009-04-18 22:09:58 +0000

    Added some experimental interface API's in nix_common_api. They are not used anywhere yet.

--- nix_common_api.py
+++ nix_common_api.py
@@ -57,3 +57,82 @@ def existsListeningNetworkSocket(ip, port, tcp):
   
   return (num > 0)
 
+
+def getAvailableInterfaces():
+  """
+  <Purpose>
+    Returns a list of available network interfaces.
+  
+  <Returns>
+    An array of string interfaces
+  """
+  # Common headers
+  # This list contains common header elements so that they can be stripped
+  headers = ["Name", "Kernel", "Iface"]
+  
+  # Netstat will return all interfaces, but also has some duplication
+  # Sed will filter the output to only return the interface names
+  # Uniq, is somewhat obvious, it will only return the unique interfaces to remove duplicates
+  cmd = "netstat -i | sed -e 's/^\\([a-zA-Z0-9]\\{1,\\}\\)\\([ ^I]*\\)\\(.*\\)$/\\1/' | uniq"
+
+  # Launch up a shell, get the feed back
+  process = subprocess.Popen(cmd, stdout=subprocess.PIPE, shell=True, close_fds=True)
+
+  # Get the output
+  output = process.stdout.readlines()
+  
+  # Close the pipe
+  process.stdout.close()
+  
+  # Create an array for the interfaces
+  interfaces = []
+  
+  for line in output:
+    # Strip the newline
+    line = line.strip("\n")
+    # Check if this is a header
+    if line in headers:
+      continue
+    interfaces.append(line)
+  
+  # Done, return the interfaces
+  return interfaces
+
+
+def getInterfaceIPAddress(interfaceName):
+  """
+  <Purpose>
+    Returns the IP address associated with the interface.
+  
+  <Arguments>
+    interfaceName: The string name of the interface, e.g. eth0
+  
+  <Returns>
+    A list of IP addresses associated with the interface.
+  """
+  # We use ifconfig with the interface name, redirect errors to null so as not to mess us up
+  cmd = "/sbin/ifconfig "+interfaceName.strip()+" 2>/dev/null "
+  cmd += "| grep inet " # Simple grep to reduce to lines that include inet
+  # This complicated sed expression extracts the first IP address on the line, and ignores everything else
+  cmd += "| sed -n -e 's|\\([a-zA-Z ^I]*[a-zA-Z][ ^I:]\\)\\([0-9]\\{1,3\\}[\\.][0-9]\\{1,3\\}[\\.][0-9]\\{1,3\}[\\.][0-9]\\{1,3\\}\\)\\{1,\\}.*|\\2|p'"
+
+  # Launch up a shell, get the feed back
+  process = subprocess.Popen(cmd, stdout=subprocess.PIPE, shell=True, close_fds=True)
+
+  # Get the output
+  output = process.stdout.readlines()
+  
+  # Close the pipe
+  process.stdout.close()
+  
+  # Create an array for the ip's
+  ipaddrs = []
+  
+  for line in output:
+     # Strip the newline and any spacing
+     line = line.strip("\n\t ")
+     ipaddrs.append(line)
+
+  # Done, return the interfaces
+  return ipaddrs
+
