24351ee5fc3c829056aeb732b9a17add9151e6e8
Justin <jcappos@nyu.edu>
2009-06-19 22:26:52 +0000

    Ticket #475.   Disallows exit and quit from repy.   They don't work well and are likely to result in programmer frustration if used.

--- repy.py
+++ repy.py
@@ -97,6 +97,9 @@ safe._BUILTIN_OK.append("setattr")
 # needed to allow primitive marshalling to be built
 safe._BUILTIN_OK.append("ord")
 safe._BUILTIN_OK.append("chr")
+# should not be used!   Use exitall instead.
+safe._BUILTIN_OK.remove("exit")
+safe._BUILTIN_OK.remove("quit")
 # __repr__ should be harmless, but do we really want to add it?
 safe._STR_OK.append("__repr__")
 # allow __ in strings.   I'm 99% sure this is okay (do I want to risk it?)
