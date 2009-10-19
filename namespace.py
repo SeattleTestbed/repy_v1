ba66f15c4884c854a7cf7582907f30731271d7ab
jsamuel <USER@DOMAIN>
2009-10-19 19:52:49 +0000

    Make object wrappers used by the namespace be hashable to the same values as the underlying wrapped object. This makes two different object wrappers instances that wrap the same object end up as the same dictionary key (when used that way) as each other and as the underlying object.

--- namespace.py
+++ namespace.py
@@ -1016,8 +1016,31 @@ class NamespaceObjectWrapper(object):
 
 
   def __repr__(self):
-    return "<Namespace wrapped " + self._wrapped__type_name + ">"
+    return "<Namespace wrapped " + self._wrapped__type_name + ": " + repr(self._wrapped__object) + ">"
 
+
+
+  def __hash__(self):
+    return self._wrapped__object.__hash__()
+
+
+
+  def __eq__(self, other):
+    """In addition to __hash__, this is necessary for use as dictionary keys."""
+    # We could either assume "other" is a wrapped object and try to compare
+    # its wrapped object against this wrapped object, or we could just compare
+    # the hashes of each. If we try to unwrap the other object, it means you
+    # couldn't compare a wrapped object to an unwrapped one.
+    return self.__hash__() == other.__hash__()
+
+
+
+  def __ne__(self, other):
+    """
+    It's good for consistency to define __ne__ if one is defining __eq__,
+    though this is not needed for using objects as dictionary keys.
+    """
+    return hash(self) != hash(other)
 
 
 
--- tests/py_z_test_namespace_wrapped_objects.py
+++ tests/py_z_test_namespace_wrapped_objects.py
@@ -116,3 +116,11 @@ try:
 except ValueError:
   pass
 
+# Make sure wrapped commhandle objects expose the hash value of the underlying
+# string that is wrapped. For an object to be used as a dictionary key, an
+# equality check is also done, so test that as well.
+assert(hash(wrappedcommhandle) == hash(commhandle))
+assert(wrappedcommhandle == commhandle)
+assert(hash(wrappedtimerhandle) == hash(timerhandle))
+assert(wrappedtimerhandle == timerhandle)
+
