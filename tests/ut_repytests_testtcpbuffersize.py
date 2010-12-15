8a08d7e4395befd31dafe0fe61aaf23545dbdffb
Justin <jcappos@nyu.edu>
2010-12-15 02:35:35 +0000

    Fix for #958.   Making the test fail only when warranted.

--- tests/ut_repytests_testtcpbuffersize.py
+++ tests/ut_repytests_testtcpbuffersize.py
@@ -1,5 +1,5 @@
 
-#pragma repy restrictions.fastestnetsend
+#pragma repy restrictions.fastestnetsendfullcpu
 
 myip = getmyip()
 
@@ -8,11 +8,11 @@ mycontext['maxlag'] = 0
 def sendforever(sockobj):
   while True:
     # send a message that is around 100 bytes
-    sockobj.send("%9f "%getruntime())
+    sockobj.send("%9f "%getruntime() + " "*90)
 
 def handleconnection(ip, port, connobj, ch, mainch):
   while True:
-    sendtime = float(connobj.recv(10).split()[0])
+    sendtime = float(connobj.recv(100).strip().split()[0])
     lag = getruntime() - sendtime
 
     if mycontext['maxlag'] < lag:
@@ -22,7 +22,7 @@ def handleconnection(ip, port, connobj, ch, mainch):
 
 
 def check_and_exit():
-  if mycontext['maxlag'] > 4:
+  if mycontext['maxlag'] > 2:
     print "TCP packets lag too long in the buffer: ", mycontext['maxlag']
 
   if mycontext['maxlag'] == 0:
